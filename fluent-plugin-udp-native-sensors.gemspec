f2382f74ccd6a19fddac50014480f115d3888588
asteen-nexcess <asteen@nexcess.net>
2019-02-14 15:16:00 -0500

    updated to latest

--- fluent-plugin-udp-native-sensors.gemspec
+++ fluent-plugin-udp-native-sensors.gemspec
@@ -17,7 +17,7 @@ Gem::Specification.new do |s|
   s.test_files    = s.files.grep(%r{^(test|spec|features)/})
   s.require_paths = %w(lib)
 
-  s.add_runtime_dependency 'fluentd', '~> 1.3', '>= 1.3.3-1'
+  s.add_runtime_dependency 'fluentd', '~> 1.2', '>= 1.2.3'
   s.add_runtime_dependency "protobuf"
   s.add_development_dependency "rake"
   s.add_runtime_dependency "influxdb", ">= 0.2.0", '<0.4.0'
--- lib/fluent/plugin/parser_juniper_udp_native.rb
+++ lib/fluent/plugin/parser_juniper_udp_native.rb
@@ -21,16 +21,9 @@ require 'npu_memory_utilization.pb.rb'
 require 'port_exp.pb.rb'
 require 'packet_stats.pb.rb'
 require 'optics.pb.rb'
-require 'ipsec_telemetry.pb.rb'
-require 'session_telemetry.pb.rb'
-require 'sr_stats_per_if_egress.pb.rb'
-require 'sr_stats_per_if_ingress.pb.rb'
-require 'sr_stats_per_sid.pb.rb'
-require 'svcset_telemetry.pb.rb'
+require 'port.pb.rb'
 require 'socket'
 require 'json'
-require 'google/protobuf/descriptor.pb'
-
 
 module Fluent
   module Plugin
@@ -56,29 +49,6 @@ module Fluent
       def parse(text)
 
         host = Socket.gethostname
-        supported_sensor_list = [
-          "jnpr_cmerror_data_ext", 
-          "jnpr_cmerror_ext", 
-          "jnpr_firewall_ext", 
-          "jnpr_lsp_statistics_ext", 
-          "jnpr_npu_utilization_ext", 
-          "jnpr_optics_ext", 
-          "jnpr_packet_statistics_ext", 
-          "jnpr_interface_exp_ext", 
-          "jnpr_interface_ext", 
-          "jnpr_qmon_ext",
-          "jnprLogicalInterfaceExt",
-          "cpu_memory_util_ext",
-          "fabricMessageExt",
-          "inline_jflow_stats_ext",
-          "npu_memory_ext",
-          "jnpr_sr_stats_per_if_egress_ext",
-          "jnpr_sr_stats_per_if_ingress_ext",
-          "jnpr_sr_stats_per_sid_ext",
-          "jnprScvsSessionExt",
-          "jnprScvsInfraExt",
-          "jnprIPsecVPNExt"
-        ]
 
         ## Decode GBP packet
         jti_msg =  TelemetryStream.decode(text)
@@ -89,51 +59,38 @@ module Fluent
         device_name = jti_msg.system_id
         yield_time = epoc_to_sec(jti_msg.timestamp)
         gpb_time = epoc_to_ms(jti_msg.timestamp)
-        $log.debug gpb_time
         measurement_prefix = "enterprise.juniperNetworks"
 
         ## Extract sensor
         begin
           jnpr_sensor = jti_msg.enterprise.juniperNetworks
           datas_sensors = JSON.parse(jnpr_sensor.to_json)
-          $log.debug  "Extract sensor data from #{device_name} with output #{output_format}"
         rescue => e
-          $log.warn   "Unable to extract sensor data sensor from jti_msg.enterprise.juniperNetworks, Error during processing: #{$!}"
-          $log.debug  "Unable to extract sensor data sensor from jti_msg.enterprise.juniperNetworks, Data Dump : " + jti_msg.inspect.to_s
           return
         end
 
         ## Go over each Sensor
         final_data = Array.new
-        supported_sensor_list.each do |sensor|
-            if jnpr_sensor.send(sensor).nil?
-                next
+        datas_sensors.each do |sensor, s_data|
+            if s_data.is_a? Hash
+                final_data = parse_hash(s_data, jnpr_sensor)
+                if final_data[0].is_a? Hash
+                    final_data = final_data
+                else
+                    final_data = final_data[0]
+                end
             end
-            final_data = parse_each_field(jnpr_sensor.send(sensor))
-            if final_data[0].is_a? Hash
-                final_data = final_data
-            else
-                final_data = final_data[0]
-            end
-
         end
-        seq = 0 
+
+        
         for data in final_data
             data['device'] = device_name
             data['host'] = host
             data['sensor_name'] = datas_sensors.keys[0]
             data['time'] = gpb_time
-            if not data.key?('key_fields')
-                data['key_fields'] = {'_seq': seq}
-                seq += 1
-            elsif data['key_fields'].empty?
-                data['key_fields'] = {'_seq': seq}
-                seq += 1
-            end
         end
 
         for data in final_data
-	    $log.debug data
             yield yield_time, data
         end
 
